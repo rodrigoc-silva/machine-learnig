a80fcdc6dffe2638376ec03f335ee2f4cf9c1270
rodrigoc-silva <carvalhaes.silva.rodrigo@gmail.com>
2021-02-05 20:03:00 -0800

    Update pandas-lab.py
    introducing covariance, correlation, concat and appned, and join functions.

--- pandas/pandas-lab.py
+++ pandas/pandas-lab.py
@@ -34,3 +34,63 @@ print(df)
 # third quartile, max on each column
 df = pd.read_csv('iris.csv')
 print(df.describe())
+
+# cov() - Covariance indicates how two variables are related. A positive
+# covariance means the variables are positively related, while a negative
+# covariance means the variables are inversely related. Drawback of covariance
+# is that it does not tell you the degree of positive or negative relation
+# creating covariance on dataframe
+df = pd.read_csv('iris.csv')
+print(df.cov())
+
+# corr() - correlation tells you the degree to which the
+# variables tend to move together. You will always talk about
+# correlation as a range between -1 and 1.
+# creating correlation matrix on dataframe
+df = pd.read_csv('iris.csv')
+print(df.corr())
+
+# merge / join
+# concat or append operation
+data = {
+        'emp_id': ['1', '2', '3', '4', '5'],
+        'first_name': ['Jason', 'Andy', 'Allen', 'Alice', 'Amy'],
+        'last_name': ['Larkin', 'Jacob', 'A', 'AA', 'Jackson']}
+df_1 = pd.DataFrame(data, columns=['emp_id', 'first_name', 'last_name'])
+
+data = {
+        'emp_id': ['4', '5', '6', '7'],
+        'first_name': ['Brian', 'Shize', 'Kim', 'Jose'],
+        'last_name': ['Alexander', 'Suma', 'Mike', 'G']}
+df_2 = pd.DataFrame(data, columns=['emp_id', 'first_name', 'last_name'])
+
+# using concat
+df = pd.concat([df_1, df_2])
+print(df)
+
+# using append
+print(df_1.append(df_2))
+
+# join the two dataframes along columns
+print(pd.concat([df_1, df_2], axis=1))
+
+# merge two dataframes based on the emp_id value
+# in this case only the emp_id's present in both table will be joined
+print(pd.merge(df_1, df_2, on='emp_id'))
+
+# left join
+print(pd.merge(df_1, df_2, on='emp_id', how='left'))
+
+# merge while adding a suffix to duplicate column names of both table
+print(pd.merge(df_1, df_2, on='emp_id', how='left', suffixes=('_left', '_right')))
+
+# right join
+print(pd.merge(df_1, df_2, on='emp_id', how='right'))
+
+# inner join
+print(pd.merge(df_1, df_2, on='emp_id', how='inner'))
+
+# outer join two frames
+print(pd.merge(df_1, df_2, on='emp_id', how='outer'))
+
+# pg98 grouping
