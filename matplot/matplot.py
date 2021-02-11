bb583a21954b32cef5418350577bec513821edda
rodrigoc-silva <carvalhaes.silva.rodrigo@gmail.com>
2021-02-10 21:44:22 -0800

    Create matplot.py
    creating first graphs on matplot

---
+++ matplot/matplot.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+# matplot
+# creating plot on variables
+# simple bar and scatter plot
+x = np.arange(5) # assume there are 5 students
+y = (20, 35, 30, 35, 27) # their test scores
+plt.bar(x, y) # bar plot
+plt.show()
+plt.scatter(x, y)
+plt.show() # scatter plot
+
+# pg 101 creating plot on data frame
