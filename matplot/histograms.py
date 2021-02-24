5873c0c4315240cc90f6696a8b35b1b9f693ece8
rodrigoc-silva <carvalhaes.silva.rodrigo@gmail.com>
2021-02-23 20:20:57 -0800

    Lab Histograms

--- matplot/histograms.py
+++ matplot/histograms.py
@@ -3,7 +3,22 @@ import pandas as pd
 
 plt.style.use('fivethirtyeight')
 
-ages = [18, 19, 21, 25, 26, 26, 30, 32, 38, 45, 55]
+# ages = [18, 19, 21, 25, 26, 26, 30, 32, 38, 45, 55]
+
+data = pd.read_csv('dataAge.csv')
+ids = data['Responder_id']
+ages = data['Age']
+
+bins = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+plt.hist(ages, bins=bins, edgecolor='black', log=True)
+
+median_age = 29
+color = '#fc4f30'
+
+plt.axvline(median_age, color=color, label='Age Median', linewidth=2)
+
+plt.legend()
 
 plt.title('Age of Respondents')
 plt.xlabel('Ages')
